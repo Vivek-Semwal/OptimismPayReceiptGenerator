d6ee20620f087bdc3f0ae795e2775a592b8b4117
Vivek-Semwal <83372536+Vivek-Semwal@users.noreply.github.com>
2025-05-16 23:16:22 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiKey: ''
+  apiKey: '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'
 };
